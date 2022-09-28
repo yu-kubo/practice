47c102492a95595a069266e7696f63c136abc13a
yu.kubo <yu.kubo@tcdigital.jp>
2022-09-28 12:03:39 +0900

    タイトルアニメーション追加、共通パーツをjQueryでするために、commonディレクトリの追加

--- static/js/home.js
+++ static/js/home.js
@@ -3,5 +3,5 @@
 $("#title-canvas").particleText({
   text: "Pixel<br>Normalize<br>Apart", // 表示させたいテキスト。改行の場合は<br>追加
   colors: ["#F54064","#F5D940", "#18EBF2"], // パーティクルの色を複数指定可能
-  speed: "middle",  // slow, middle, high の3つから粒子が集まる速さを選択
+  speed: "slow",  // slow, middle, high の3つから粒子が集まる速さを選択
 });
